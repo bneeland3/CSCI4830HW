411f9d0bf72050560a5737c445c52ef1958b28eb
Brenna Neeland <brne3762@colorado.edu>
2024-02-15 15:38:43 -0700

    analytical soln

--- hw2/sis.py
+++ hw2/sis.py
@@ -25,23 +25,30 @@ def forward_euler_solver(beta, gamma, s_0, i_0, delta_t, t_final):
     suceptibles = [s]
     infected = [i]
     time = [t]
+    analytical = [i]
     
     ind = 1
     
     while t < t_final:
         ## logistic ODE
         i_t = (beta - gamma)*infected[ind-1]*(1-(infected[ind-1]/(1-(1/R_0))))
-        ## closed form soln
-        # if np.isclose(i, 1):  # Check if i is close to 1 to avoid division by zero
-        #     i_t = 1
-        # else:
+        
+        ## closed form soln -- analytical
+        exponential_term = np.exp(-(beta - gamma) * t)
+        analytical_soln = ((1 - (1 / R_0)) / (1 + ((1 - (1 / R_0)) - i_0) / i_0 * exponential_term))
+
+        # if R_0 != 0:  # Check to avoid division by zero
         #     exponential_term = np.exp(-(beta - gamma) * t)
-        #     fraction_term = ((1 - (1 / R_0)) / (1 + (1 - (1 / R_0) - i_0) / (R_0 * i_0)))
-        #     i_t = 1 - fraction_term * i_0 * exponential_term
+        #     analytical_soln = (1 - (1 / R_0)) / (1 + (1 - (1 / R_0) - i_0) / (R_0 - i_0)) * i_0 * exponential_term
+        # else:
+        #     analytical_soln = 1  # Handle division by zero by setting analytical solution to 1
 
         # appends to lists
         i_next = (i_t * delta_t) + infected[ind-1]
+        a_next = (analytical_soln * delta_t) + analytical[ind-1]
+        
         infected.append(i_next)
+        analytical.append(analytical_soln)
         
         # updates time
         t += delta_t
@@ -50,9 +57,11 @@ def forward_euler_solver(beta, gamma, s_0, i_0, delta_t, t_final):
         # updates current SIS values to the new step
         i =  i_t
         
+        # updates index
         ind += 1
         
-
     print(infected)
+    print(analytical)
+    
     # returns a list of lists
-    return [suceptibles, infected, time]
+    return [suceptibles, infected, time, analytical]
