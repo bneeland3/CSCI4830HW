ce4c702802f2acfa3f10cc2cdbb48cd44fd16529
Brenna Neeland <brne3762@colorado.edu>
2024-02-19 22:35:47 -0700

    fixes for edt

--- hw2/run_edt.sh
+++ hw2/run_edt.sh
@@ -3,7 +3,7 @@ set -e # -e stops on error.
 set -u # -u raises error.
 set -o pipefail # Fails if a prior step has failed.
 
-echo '...running plot_edt.py...'
+echo '...running plot_edt.py without graph...'
 python plot_edt.py \
     --beta 3.0 \
     --gamma 2.0 \
@@ -13,3 +13,16 @@ python plot_edt.py \
     --t_final 25 \
     --output_file E_delta_t.png \
     --E_delta_t True \
+    --graph_E False \
+
+echo '...running plot_edt.py with graph...'
+python plot_edt.py \
+    --beta 3.0 \
+    --gamma 2.0 \
+    --s_0 0.99 \
+    --i_0 0.01 \
+    --delta_t 0.5 \
+    --t_final 25 \
+    --output_file E_delta_t.png \
+    --E_delta_t False \
+    --graph_E True \
