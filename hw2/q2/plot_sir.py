e908fa8475be0ef5cabb98777020740b15f9463b
Brenna Neeland <brne3762@colorado.edu>
2024-02-22 17:51:50 -0700

    bar plots and fixes

--- hw2/q2/plot_sir.py
+++ hw2/q2/plot_sir.py
@@ -30,17 +30,21 @@ def main():
 
     args = parser.parse_args()
     
+    # Run LM and ANM simulations
     lm_data = forward_euler_solver_lm(args.R_0, args.N, args.VE)
     anm_data = forward_euler_solver_anm(args.R_0, args.N, args.VE)
-    lm_cumulative_infectious = round(lm_data[5],0)
-    anm_cumulative_infectious = round(anm_data[6],0)
-    # print(f'cumulative infectious for lm : {lm_cumulative_infectious}')
-    # print(f'cumulative infectious for anm : {anm_cumulative_infectious}')
+    
+    # Extract cumulative infectious populations
+    lm_cumulative_infectious = round(lm_data[5], 0)  # Last value of infected from LM data
+    anm_cumulative_infectious = round(anm_data[6], 0)  # Last value of infected from ANM data
+    
+    print(f'Cumulative infectious for LM: {lm_cumulative_infectious}')
+    print(f'Cumulative infectious for ANM: {anm_cumulative_infectious}')
     
     # Plotting
     fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 5))
 
-    # Original Plots
+    # SIR dynamic curves 
     ax1.plot(lm_data[0], lm_data[2], color='red', label='Infected-LM')
     ax1.plot(anm_data[0], anm_data[2], color='blue', label='Infected-ANM')
     ax1.set_xlabel('Time')
@@ -48,7 +52,7 @@ def main():
     ax1.set_title(f'Infected Population in SIR Model Simulation of {args.R_0}')
     ax1.legend()
 
-    # New Plots
+    # Bar Plots
     ax2.bar(['LM', 'ANM'], [lm_cumulative_infectious, anm_cumulative_infectious], color=['red', 'blue'])
     ax2.set_ylabel('Cumulative Infectious Population')
     ax2.set_title('Cumulative Infectious Population')
